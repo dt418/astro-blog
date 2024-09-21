193cf9326e2f4886dfafe62e7948ba4784a9249a
thanhd <danhthanh418@gmail.com>
2024-09-21 09:50:51 +0700

    up: update taiilwindcss config with type

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import typographyPlugin from '@tailwindcss/typography';
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
